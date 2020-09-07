5c96e0c0c3a0d33df378557d55b98e8e99eb3164
Armando Roman <armando.roman.jr20@gmail.com>
2020-09-07 16:52:18 -0500

    set up the controller to display the view for the homepage

---
+++ src/main/java/gachostyle/magazine/controllers/home.java
@@ -0,0 +1,13 @@
+package gachostyle.magazine.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class home {
+
+    @GetMapping("/")
+    public String gachoHome(){
+        return "Gacho-home";
+    }
+}
