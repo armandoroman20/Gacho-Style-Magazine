57f23939f18052c76cf313c860a84f2c93105000
Armando Roman <armando.roman.jr20@gmail.com>
2020-09-07 16:27:14 -0500

    pushing html

---
+++ src/main/java/gachostyle/magazine/GachoStyleMagazineApplication.java
@@ -0,0 +1,13 @@
+package gachostyle.magazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachoStyleMagazineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachoStyleMagazineApplication.class, args);
+    }
+
+}
