1f253bb824fa2c15865b67d08ab07e64426433b4
Armando <armando.roman.jr20@gmail.com>
2020-12-04 23:54:48 -0600

    changes

--- src/main/java/gachostyle/magazine/GachoStyleMagazineApplication.java
+++ src/main/java/gachostyle/magazine/GachoStyleMagazineApplication.java
@@ -2,12 +2,18 @@ package gachostyle.magazine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GachoStyleMagazineApplication {
+public class GachoStyleMagazineApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GachoStyleMagazineApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GachoStyleMagazineApplication.class);
+    }
 }
