263116c6a33758ac6f5d3f7b036eda7aaaee2620
Armando <armando.roman.jr20@gmail.com>
2021-01-24 23:25:18 -0600

    I can insert into the DB

--- src/main/java/gachostyle/magazine/controllers/Art.java
+++ src/main/java/gachostyle/magazine/controllers/Art.java
@@ -4,9 +4,11 @@ import gachostyle.magazine.models.Image;
 import gachostyle.magazine.models.User;
 import gachostyle.magazine.repositories.ImageRepository;
 import gachostyle.magazine.repositories.Users;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -21,29 +23,21 @@ public class Art {
         this.imageDao = imageDao;
     }
 
-    @GetMapping("/art")
-    public String artPage(Model model) {
-
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        model.addAttribute("newImage", new Image());
-
-//        User admin = userDao.getOne(loggedInUser.getId());
-//        model.addAttribute("admin", admin);
-        return "art";
-    }
-
-
-    @PostMapping("/art")
-    public String saveImage(@RequestParam(name = "ownerId") long ownerId, @ModelAttribute Image image){
-        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//    @GetMapping("/art")
+//    public String artPage(Model model) {
+//
+//        model.addAttribute("artImages", imageDao.findAll());
+////        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//
+//        model.addAttribute("newImage", new Image());
+//
+////        User admin = userDao.getOne(loggedInUser.getId());
+////        model.addAttribute("admin", admin);
+//        return "art";
+//    }
 
 
-        if(loggedInUser.getId() == 1){
-            imageDao.save(image);
-        }
-     return "/art";
-    }
+
 
 //    @GetMapping("/art/{id}")
 //    public String artForm(@PathVariable("id") long id,Model model) {
@@ -58,4 +52,27 @@ public class Art {
 
 //        return "art" + admin;
 //    }
+
+
+    @GetMapping("/art")
+    public String showCreateForm(Model model) {
+        model.addAttribute("artImages", imageDao.findAll());
+        model.addAttribute("image", new Image());
+
+        return "/art";
+    }
+
+
+    @PostMapping("/art")
+    public String create(
+            @RequestParam(name = "description") String description, @ModelAttribute Image newImage, Model model
+    ) {
+        Image image = new Image();
+        image.setDescription(description);
+        imageDao.save(image);
+        model.addAttribute("newImage", image);
+
+        // save the ad...
+        return "art";
+    }
 }
